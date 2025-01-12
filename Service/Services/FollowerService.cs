0e85995547ca2c79a0cee7e763401dfae20a927e
sara-eisenstein <0583215222s@gmail.com>
2025-01-12 18:08:44 +0200

    fix service bh

--- Service/Services/FollowerService.cs
+++ Service/Services/FollowerService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using Repositorys.Entities;
+using Repositorys.Interface;
+using Service.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,41 @@ using System.Threading.Tasks;
 
 namespace Service.Services
 {
-    internal class FollowerService
+    public class FollowerService : IService<FollowerDto>
     {
+        private readonly IRepository<Follower> repository;
+        private readonly IMapper mapper;
+        public FollowerService(IRepository<Follower> repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+    
+        public FollowerDto Add(FollowerDto item)
+        {
+            var entity = mapper.Map<Follower>(item);
+            return mapper.Map<FollowerDto>(repository.Add(entity));
+        }
+
+        public void Delete(int id)
+        {
+           repository.Delete(id);
+        }
+
+        public FollowerDto Get(int id)
+        {
+            return mapper.Map<FollowerDto>(Get(id));
+        }
+
+        public List<FollowerDto> GetAll()
+        {
+            return mapper.Map<List<FollowerDto>>(GetAll());
+        }
+
+        public FollowerDto Update(FollowerDto item)
+        {
+            var entity = mapper.Map<Follower>(item);
+            return mapper.Map<FollowerDto>(repository.Update(entity));
+        }
     }
 }
